d2750552c599bc115458cf7c34b203cf6a3b1cda
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-07-18 16:17:49 +0400

    set documentation code

--- controls.py
+++ controls.py
@@ -11,18 +11,21 @@ def events(snake):
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                pygame.transform.rotate(snake.image, 180)
+                image = pygame.transform.rotate(snake.image, 180)
+                snake.output_snake(image)
             elif event.key == pygame.K_DOWN:
-                pygame.transform.rotate(snake.image, 180)
+                image = pygame.transform.rotate(snake.image, 180)
             elif event.key == pygame.K_RIGHT:
-                pygame.transform.rotate(snake.image, 180)
+                image = pygame.transform.rotate(snake.image, 180)
             elif event.key == pygame.K_LEFT:
-                pygame.transform.rotate(snake.image, 180)
+                image = pygame.transform.rotate(snake.image, 180)
 
 
 def turn(snake_look):
     """обработка поворотов змейки"""
     if snake_look == 'LEFT' or 'RIGHT':
+        pass
         # Can turn up or down
     elif snake_look == 'UP' or 'DOWN':
+        pass
         # Can turn left or right
--- snake.py
+++ snake.py
@@ -1,24 +1,31 @@
 import pygame
+import controls
 
 
 class Snake():
-    def __init__(self, screen):
+    """Класс игрового персонажа змейка"""
+
+    def __init__(self, background):
         """Инициализация змейки"""
 
-        self.screen = screen
+
+        """
+        Входные параметры: background - фон игры, image - изоображение змейки
+        """
+
+        self.background = background
         self.image = pygame.image.load('images/snake_head.png')
         self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.background_rect = background.get_rect()
+        self.rect.centerx = self.background_rect.centerx
+        self.rect.bottom = self.background_rect.bottom
 
     def move(self):
-        """движение змейки"""
+        """Метод движение змейки"""
         self.rect.x += 1
         if self.rect.left > 800:
             self.rect.right = 0
 
-    def output_snake(self):
-        """рисование змейки"""
-
-        self.screen.blit(self.image, self.rect)
+    def output_snake(self, turn):
+        """Метод отображения змейки"""
+        self.background.blit(turn, self.rect)
