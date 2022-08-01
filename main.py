e231ca555ed2d3f07545de213d744908e6b4ce33
Ilya Tyurin <ilya.tyurin@hotmail.com>
2022-08-01 11:50:17 +0400

    added img main menu + refactor code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pygame
+#!/usr/bin/python
+import pygame as pg
 import time
 import random
 from os import path
+
 """Оконное приложение написанное на языке Python 3.8.2 с использованием фреймворка pygame 2.1.2"""
 
 """Приложение созданно по прототипу игры змейка.
@@ -14,38 +16,35 @@ from os import path
    
    Пользователю будет предложенно начать игру заново либо выйти из игры.
    """
-pygame.init()
+pg.init()
 
 """Установим константы (R, G, B) цветов для последующего использования"""
-WHITE = (255, 255, 255)  # Белый
 YELLOW = (255, 255, 102)  # Жёлтый
 BLACK = (0, 0, 0)  # Чёрный
-RED = (213, 50, 80)  # Красный
-GREEN = (0, 255, 0)  # Зеленый
-BLUE = (50, 153, 213)  # Синий
 
 """Установим константы для ширины и высоты игрового поля"""
 WIDTH = 600  # Ширина игрового поля
 HIGHT = 400  # Высота игрового поля
 
-dis = pygame.display.set_mode((WIDTH, HIGHT))
-pygame.display.set_caption('Snake Game by Valtesar')
+dis = pg.display.set_mode((WIDTH, HIGHT))
+pg.display.set_caption('Snake Game by Valtesar')
 
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 
 SNAKE_BLOCK = 10  # Размер квадрата змейки в пикселях
 SNAKE_SPEED = 15  # Скорость передвижения змейки
 
-font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 20)
-result_score_font = pygame.font.SysFont("comicsansms", 35, bold=True)
+font_style = pg.font.SysFont("bahnschrift", 25)
+score_font = pg.font.SysFont("comicsansms", 20)
+result_score_font = pg.font.SysFont("comicsansms", 35, bold=True)
 
 """Инициализация спрайтов"""
-snake_img = pygame.image.load('images/snake_block.png')
-food_img = pygame.image.load('images/food_block.png')
-background_img = pygame.image.load('images/background.png')
-game_over_img = pygame.image.load('images/game_over.png')
-game_over_record_img = pygame.image.load('images/game_over_record.png')
+snake_img = pg.image.load('images/snake_block.png')
+food_img = pg.image.load('images/food_block.png')
+background_img = pg.image.load('images/background.png')
+game_over_img = pg.image.load('images/game_over.png')
+game_over_record_img = pg.image.load('images/game_over_record.png')
+main_menu_img = pg.image.load('images/main_menu.png')
 
 event_log = ['']
 
@@ -80,22 +79,11 @@ def output_snake(snake_block, snake_list):
         dis.blit(snake_img, [x[0], x[1], snake_block, snake_block])
 
 
-def message(msg, color):
-    """Метод вывода сообщений на экран пользователя"""
-
-    """Принимает в себя аргуементы:
-            msg -> текст сообщения
-            color -> цвет текста сообщения"""
-    """Отрисовывает на экране текст ровно по центру в зависимости от ширины и высоты игрового поля"""
-    text = font_style.render(msg, True, color)
-    dis.blit(text, [WIDTH / 15, HIGHT / 3])
-
-
 def generate_food():
     """Метод генерации случайных координат <x1> <y1> для еды змейки"""
 
     x1_food = random.randrange(0, WIDTH, 10)
-    y1_food = random.randrange(0, HIGHT, 10)
+    y1_food = random.randrange(0, HIGHT - 20, 10)
     return x1_food, y1_food
 
 
@@ -105,6 +93,7 @@ def game_loop():
     """По умолчанию параметры конца игры и закрытия окна в положении 0"""
     game_over = False
     game_close = False
+    game_start = True
 
     """Стартовое положение змейки на экране относительно координат <x1_snake> <y1_snake>"""
     x1_snake = WIDTH / 2
@@ -123,7 +112,8 @@ def game_loop():
         Округляет его до целого числа int"""
     x1_food = generate_food()[0]
     y1_food = generate_food()[1]
-
+    """Пытаемся прочитать файл high_sore.txt, если файл отсутствует,
+       то создаем файл с таким именем и записываем туда значение <0>"""
     if not path.exists('high_score.txt'):
         with open('high_score.txt', 'w') as w:
             w.write('0')
@@ -133,13 +123,25 @@ def game_loop():
     """Запускаем главный бесконечный цикл программы,
        который работает до тех пор пока пользователь не закроет окно игры"""
     while not game_close:
+        """Запускаем вложенный бесконечный цикл программы,
+           который работает до тех пор пока пользователь не нажмет ENTER или не закроет окно игры"""
+        while game_start:
+            dis.blit(main_menu_img, [0, 0])
+            pg.display.update()
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    game_close = True
+                    game_start = False
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_RETURN:
+                        game_start = False
 
-        """Запускаем вложенный бесконечный цикл програамы,
+        """Запускаем вложенный бесконечный цикл программы,
            который работает до тех пор, пока пользователь не проиграет"""
         while game_over:
             dis.blit(game_over_img, [0, 0])
             output_score(length_of_snake - 1, True, high_score)
-            pygame.display.update()
+            pg.display.update()
 
             with open('high_score.txt', 'w') as scores:
                 scores.write(str(high_score))
@@ -147,29 +149,30 @@ def game_loop():
             """Отслеживаем игровые события. 
                Нажатие клавишы ESC ведет в выходу из игры.
                Нажатие клавишы Space запускает игру заново"""
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
                     game_close = True
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
+                    game_over = False
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
                         game_over = False
                         game_close = True
-                    if event.key == pygame.K_SPACE:
+                    if event.key == pg.K_SPACE:
                         game_loop()
 
         """Отслеживаем игровые события.
            Закрытие игрового окна закрывает приложение без ошибок.
-           Нажатие клавишы влево - движение змейки влево <<< X, Y
-           Нажатие клавишы вправо - движение змейки вправо X >>>, Y 
-           Нажатие клавишы вверх - движение змейки вверх X, Y /| 
-           Нажатие клавишы вниз - движение змейки вниз X, Y |/ """
+           Нажатие клавишы влево - движение змейки влево  - X, Y
+           Нажатие клавишы вправо - движение змейки вправо X +, Y 
+           Нажатие клавишы вверх - движение змейки вверх X, Y + 
+           Нажатие клавишы вниз - движение змейки вниз X, - Y"""
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 game_close = True
-            if event.type == pygame.KEYDOWN:
+            if event.type == pg.KEYDOWN:
                 """Обработка события нажатия клавишы влево"""
-                if event.key == pygame.K_LEFT:
+                if event.key == pg.K_LEFT:
                     snake_direction = 'left'
                     if not event_log.__contains__('right'):
                         x1_change = -SNAKE_BLOCK
@@ -177,7 +180,7 @@ def game_loop():
                         event_log.append(snake_direction)
                         event_log.pop(0)
 
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pg.K_RIGHT:
                     """Обработка события нажатия клавишы вправо"""
                     snake_direction = 'right'
                     if not event_log.__contains__('left'):
@@ -186,7 +189,7 @@ def game_loop():
                         event_log.append(snake_direction)
                         event_log.pop(0)
 
-                elif event.key == pygame.K_UP:
+                elif event.key == pg.K_UP:
                     """Обработка события нажатия клавишы вверх"""
                     snake_direction = 'up'
                     if not event_log.__contains__('down'):
@@ -195,7 +198,7 @@ def game_loop():
                         event_log.append(snake_direction)
                         event_log.pop(0)
 
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pg.K_DOWN:
                     """Обработка события нажатия клавишы вниз"""
                     snake_direction = 'down'
                     if not event_log.__contains__('up'):
@@ -242,10 +245,10 @@ def game_loop():
 
         output_snake(SNAKE_BLOCK, snake_list)
         output_score(length_of_snake - 1, False, high_score)
-        pygame.display.update()
+        pg.display.update()
         clock.tick(SNAKE_SPEED)
     quit()
-    pygame.quit()
+    pg.quit()
 
 
 if __name__ == '__main__':
